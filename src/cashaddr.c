c53d9ad2943d19a5b235da6f74cd7866acc6b736
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2018-01-27 21:11:48 +0100

    Better handling of invalid parameters. Editorial changes.

--- src/cashaddr.c
+++ src/cashaddr.c
@@ -35,22 +35,22 @@ static const char *charset = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
 uint64_t cashaddr_polymod_step(uint64_t pre) {
     uint8_t b = pre >> 35;
     return ((pre & 0x07ffffffff) << 5) ^ (-((b >> 0) & 1) & 0x98f2bc8e61UL) ^
-	(-((b >> 1) & 1) & 0x79b76d99e2UL) ^ (-((b >> 2) & 1) & 0xf33e5fb3c4UL) ^
-	(-((b >> 3) & 1) & 0xae2eabe2a8UL) ^ (-((b >> 4) & 1) & 0x1e4f43e470UL);
+        (-((b >> 1) & 1) & 0x79b76d99e2UL) ^ (-((b >> 2) & 1) & 0xf33e5fb3c4UL) ^
+        (-((b >> 3) & 1) & 0xae2eabe2a8UL) ^ (-((b >> 4) & 1) & 0x1e4f43e470UL);
 }
 
 uint64_t PolyMod(uint8_t *prefix, uint8_t *payload, size_t payload_length) {
     size_t i;
     uint64_t c = 1;
     while (*prefix != 0 ) {
-	c = cashaddr_polymod_step(c) ^ (*prefix++ & 0x1f); // Prefix
+        c = cashaddr_polymod_step(c) ^ (*prefix++ & 0x1f); // Prefix
     }
     c = cashaddr_polymod_step(c); // The zero valued separator
     for (i=0; i<payload_length; ++i) {
-	c = cashaddr_polymod_step(c) ^ (*payload++); // Hash
+        c = cashaddr_polymod_step(c) ^ (*payload++); // Hash
     }
     for (i=0; i<8; ++i) {
-	c = cashaddr_polymod_step(c); // 8 zeros for empty checksum
+        c = cashaddr_polymod_step(c); // 8 zeros for empty checksum
     }
     return c ^ 1;
 }
@@ -84,28 +84,35 @@ void create_checksum(uint8_t *payload, size_t payload_length, uint8_t *checksum)
     uint64_t mod = PolyMod(prefix, payload, payload_length);
 
     for (size_t i = 0; i < 8; ++i) {
-	// Convert the 5-bit groups in mod to checksum values.
-	*checksum++ = (mod >> (5 * (7 - i))) & 0x1f;
+        // Convert the 5-bit groups in mod to checksum values.
+        *checksum++ = (mod >> (5 * (7 - i))) & 0x1f;
     }
 }
 
 int cashaddr_encode(uint8_t *hash, const size_t hash_length, uint8_t *addr,
-		    const size_t max_addr_len, const unsigned short version) {
+                    const size_t max_addr_len, const unsigned short version) {
 
-    uint8_t version_byte = CASHADDR_P2PKH;  // P2PKH
+    uint8_t version_byte;
     uint8_t checksum[8] = {0, 0, 0, 0, 0, 0, 0, 0};  // 5-bit bytes.
     uint8_t tmp[40];  // 8-bit bytes. Should be enough for 1 version byte + 160 bit
     uint8_t payload[40];  // 5-bit bytes. Should be enough for 1 version byte + 160 bit hash
+    uint8_t *addr_start;
     size_t payload_length = 0;
     size_t addr_length = 0;
     size_t i;
 
+    addr_start = addr;
+    *addr_start = 0;
+
     if (hash_length != 20) // Only support 160 bit hash
+        return 0;
+    if (version == CASHADDR_P2PKH) {  // Support P2PKH = 0, P2SH = 1
+        version_byte = 0;
+    } else if (version == CASHADDR_P2SH) {
+	version_byte = 8;
+    } else {
 	return 0;
-    if (version > 1)  // Support P2PKH = 0, P2SH = 1
-	return 0;
-    if (version == CASHADDR_P2SH)
-	version_byte = 8; // P2SH
+    }
 
     tmp[0] = version_byte;
     os_memmove(tmp+1, hash, hash_length);
@@ -114,23 +121,29 @@ int cashaddr_encode(uint8_t *hash, const size_t hash_length, uint8_t *addr,
     create_checksum(payload, payload_length, checksum); // Assume prefix is 'bitcoincash'
 
     for (i = 0; i < payload_length; ++i) {
-        if (*payload >> 5)
+        if (*payload >> 5) {
+            *addr_start = 0;
             return 0;
-	addr_length++;
-	if (max_addr_len < addr_length) {
-	    L_DEBUG_APP(("Encode overflow %d %d\n", addr_length, max_addr_len));
-	    return 0;
-	}
-	*(addr++) = charset[payload[i]];
+        }
+        addr_length++;
+        if (max_addr_len < addr_length) {
+            L_DEBUG_APP(("Encode overflow %d %d\n", addr_length, max_addr_len));
+            *addr_start = 0;
+            return 0;
+        }
+        *(addr++) = charset[payload[i]];
     }
     for (i = 0; i < 8; ++i) {
-        if (*checksum >> 5)
+        if (*checksum >> 5) {
+            *addr_start = 0;
             return 0;
-	addr_length++;
-	if (max_addr_len < addr_length) {
-	    L_DEBUG_APP(("Encode overflow %d %d\n", addr_length, max_addr_len));
-	    return 0;
-	}
+        }
+        addr_length++;
+        if (max_addr_len < addr_length) {
+            L_DEBUG_APP(("Encode overflow %d %d\n", addr_length, max_addr_len));
+            *addr_start = 0;
+            return 0;
+        }
         *(addr++) = charset[checksum[i]];
     }
     *addr = 0;
