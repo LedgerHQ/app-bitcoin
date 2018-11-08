956f198db3932ecc2114fbbe376932fd0fbab771
Traysi <link@traysi.org>
2018-11-08 14:46:39 -0600

    Added Ravencoin

--- include/btchip_context.h
+++ include/btchip_context.h
@@ -268,7 +268,8 @@ typedef enum btchip_coin_kind_e {
     COIN_KIND_BITCOIN_PRIVATE,
     COIN_KIND_HORIZEN,
     COIN_KIND_GAMECREDITS,
-    COIN_KIND_ZCOIN
+    COIN_KIND_ZCOIN,
+    COIN_KIND_RVN
 } btchip_coin_kind_t;
 
 typedef struct btchip_altcoin_config_s {
