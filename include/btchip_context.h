fcd68ad3617ac95195d32ae85529c8f74ad970b3
TheCreator <31379149+Flurbos@users.noreply.github.com>
2018-11-25 08:49:16 +0800

    Update btchip_context.h
    Fix typo

--- include/btchip_context.h
+++ include/btchip_context.h
@@ -269,7 +269,7 @@ typedef enum btchip_coin_kind_e {
     COIN_KIND_HORIZEN,
     COIN_KIND_GAMECREDITS,
     COIN_KIND_ZCOIN,
-	COIN_KIND_UNOBTNIUM
+    COIN_KIND_UNOBTANIUM
 } btchip_coin_kind_t;
 
 typedef struct btchip_altcoin_config_s {
