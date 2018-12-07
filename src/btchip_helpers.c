f90c1a4ebd1ec2c74ce355f6a94da8becad318ad
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2018-12-07 18:59:20 +0100

    whitelist P2WPKH purpose in change path guard

--- src/btchip_helpers.c
+++ src/btchip_helpers.c
@@ -348,7 +348,8 @@ unsigned char bip44_change_path_guard(unsigned char WIDE *bip32Path) {
     // If the path length is not compliant with BIP44 or if the purpose/coin type don't match regular usage
     if(path_len != BIP44_PATH_LEN ||
        ((bip32PathInt[BIP44_PURPOSE_OFFSET]^0x80000000) != 44 &&
-       (bip32PathInt[BIP44_PURPOSE_OFFSET]^0x80000000) != 49)) {
+       (bip32PathInt[BIP44_PURPOSE_OFFSET]^0x80000000) != 49 &&
+       (bip32PathInt[BIP44_PURPOSE_OFFSET]^0x80000000) != 84)) {
         return 1;
     }
 
