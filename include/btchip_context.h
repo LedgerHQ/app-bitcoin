3d5d9fefb6b416fe50fb7bedc98c9c886e250ad7
BadChoicesZ <zaqps4@gmail.com>
2018-12-15 16:16:04 +0800

    Revert "Fix Conflicts"
    This reverts commit 5f1505938ec09fc13ed0a5340d3c9bafb50cb533.
    test

--- include/btchip_context.h
+++ include/btchip_context.h
@@ -85,7 +85,6 @@ enum btchip_output_parsing_state_e {
     BTCHIP_OUTPUT_PARSING_NUMBER_OUTPUTS = 0x01,
     BTCHIP_OUTPUT_PARSING_OUTPUT = 0x02,
     BTCHIP_OUTPUT_FINALIZE_TX = 0x03,
-    BTCHIP_BIP44_CHANGE_PATH_VALIDATION = 0x04,
     BTCHIP_OUTPUT_HANDLE_LEGACY = 0xFF
 };
 typedef enum btchip_output_parsing_state_e btchip_output_parsing_state_t;
@@ -270,7 +269,6 @@ typedef enum btchip_coin_kind_e {
     COIN_KIND_HORIZEN,
     COIN_KIND_GAMECREDITS,
     COIN_KIND_ZCOIN,
-    COIN_KIND_ZCLASSIC,
     COIN_KIND_UNOBTANIUM
 } btchip_coin_kind_t;
 
