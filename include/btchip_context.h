e350c2ca82f7c78f0c04369028736d26f25c3d91
BTChip github <contact@btchip.com>
2019-01-22 14:43:29 +0100

    Merge PR #56 and bump version

--- include/btchip_context.h
+++ include/btchip_context.h
@@ -290,7 +290,8 @@ typedef enum btchip_coin_kind_e {
     COIN_KIND_HORIZEN,
     COIN_KIND_GAMECREDITS,
     COIN_KIND_ZCOIN, 
-    COIN_KIND_ZCLASSIC
+    COIN_KIND_ZCLASSIC,
+    COIN_KIND_XSN
 } btchip_coin_kind_t;
 
 typedef struct btchip_altcoin_config_s {
