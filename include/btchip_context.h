aa1ceb49137a1be8d14f844c8dcf7fee19e3515e
omotenashicoin-project <git@omotenashicoin.site>
2020-02-16 14:51:21 +0900

    Added MTNS

--- include/btchip_context.h
+++ include/btchip_context.h
@@ -292,7 +292,8 @@ typedef enum btchip_coin_kind_e {
     COIN_KIND_XSN,
     COIN_KIND_NIX,
     COIN_KIND_LBRY,
-    COIN_KIND_RESISTANCE
+    COIN_KIND_RESISTANCE,
+    COIN_KIND_MTNS
 } btchip_coin_kind_t;
 
 typedef struct btchip_altcoin_config_s {
