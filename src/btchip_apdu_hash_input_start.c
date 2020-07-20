65b7423a3fe60bbdee9e1b2b389c5aeecd61b9b0
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-07-20 15:07:19 +0200

    Remove trusted input warning for swap

--- src/btchip_apdu_hash_input_start.c
+++ src/btchip_apdu_hash_input_start.c
@@ -130,6 +130,11 @@ unsigned short btchip_apdu_hash_input_start() {
         && IS_INPUT() 
         && !IS_INPUT_TRUSTED())
     {
+        if(btchip_context_D.called_from_swap){
+            /* There is no point in displaying a warning when the app is signing
+            in silent mode, as its UI is hidden behind the exchange app*/
+            return BTCHIP_SW_SWAP_WITHOUT_TRUSTED_INPUTS;
+        }
         btchip_context_D.segwitWarningSeen = 1;
         btchip_context_D.io_flags |= IO_ASYNCH_REPLY;
         btchip_bagl_request_segwit_input_approval();
