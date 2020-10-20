57e288def41364f2e806e48ff47abf832ac26cac
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-10-20 17:00:04 +0200

    Use recommanded UX macros from recents SDKs

--- src/main.c
+++ src/main.c
@@ -205,7 +205,7 @@ UX_STEP_NOCB(
       "Application",
       "is ready",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_idle_flow_2_step,
     pb,
     ux_menulist_init(0, settings_submenu_getter, settings_submenu_selector),
@@ -220,7 +220,7 @@ UX_STEP_NOCB(
       "Version",
       APPVERSION,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_idle_flow_4_step,
     pb,
     os_sched_exit(-1),
@@ -252,7 +252,7 @@ UX_STEP_NOCB(
       .title = "Message hash",
       .text = vars.tmp.fullAddress,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_sign_flow_3_step,
     pbb,
     io_seproxyhal_touch_message_signature_verify_ok(NULL),
@@ -261,7 +261,7 @@ UX_STEP_VALID(
       "Sign",
       "message",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_sign_flow_4_step,
     pbb,
     io_seproxyhal_touch_message_signature_verify_cancel(NULL),
@@ -308,7 +308,7 @@ UX_STEP_NOCB(
       .title = "Fees",
       .text = vars.tmp.feesAmount,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_confirm_full_flow_5_step,
     pbb,
     io_seproxyhal_touch_verify_ok(NULL),
@@ -317,7 +317,7 @@ UX_STEP_VALID(
       "Accept",
       "and send",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_confirm_full_flow_6_step,
     pb,
     io_seproxyhal_touch_verify_cancel(NULL),
@@ -359,7 +359,7 @@ UX_STEP_NOCB(
       .title = "Address",
       .text = vars.tmp.fullAddress,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_confirm_single_flow_5_step,
     pb,
     io_seproxyhal_touch_verify_ok(NULL),
@@ -367,7 +367,7 @@ UX_STEP_VALID(
       &C_icon_validate_14,
       "Accept",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_confirm_single_flow_6_step,
     pb,
     io_seproxyhal_touch_verify_cancel(NULL),
@@ -401,7 +401,7 @@ UX_STEP_NOCB(
       .title = "Fees",
       .text = vars.tmp.feesAmount,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_finalize_flow_5_step,
     pbb,
     io_seproxyhal_touch_verify_ok(NULL),
@@ -410,7 +410,7 @@ UX_STEP_VALID(
       "Accept",
       "and send"
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_finalize_flow_6_step,
     pb,
     io_seproxyhal_touch_verify_cancel(NULL),
@@ -442,7 +442,7 @@ UX_STEP_NOCB(
       .title = "Derivation path",
       .text = vars.tmp_warning.derivation_path,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_display_public_flow_3_step,
     pnn,
     io_seproxyhal_touch_display_cancel(NULL),
@@ -466,7 +466,7 @@ UX_STEP_NOCB(
       .title = "Address",
       .text = G_io_apdu_buffer+200,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_display_public_flow_6_step,
     pb,
     io_seproxyhal_touch_display_ok(NULL),
@@ -474,7 +474,7 @@ UX_STEP_VALID(
       &C_icon_validate_14,
       "Approve",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_display_public_flow_7_step,
     pb,
     io_seproxyhal_touch_display_cancel(NULL),
@@ -502,7 +502,7 @@ UX_FLOW(ux_display_public_flow,
 
 
 //////////////////////////////////////////////////////////////////////
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_display_token_flow_1_step,
     pbb,
     io_seproxyhal_touch_display_ok(NULL),
@@ -511,7 +511,7 @@ UX_STEP_VALID(
       "Confirm token",
       G_io_apdu_buffer+200,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_display_token_flow_2_step,
     pb,
     io_seproxyhal_touch_display_cancel(NULL),
@@ -526,7 +526,7 @@ UX_FLOW(ux_display_token_flow,
 );
 
 //////////////////////////////////////////////////////////////////////
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_pubkey_approval_flow_1_step,
     pbb,
     io_seproxyhal_touch_display_ok(NULL),
@@ -535,7 +535,7 @@ UX_STEP_VALID(
       "Export",
       "public key?",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_pubkey_approval_flow_2_step,
     pb,
     io_seproxyhal_touch_display_cancel(NULL),
@@ -565,7 +565,7 @@ UX_STEP_NOCB(
       .title = "Change path",
       .text = vars.tmp_warning.derivation_path,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_change_path_approval_flow_3_step,
     pbb,
     io_seproxyhal_touch_display_cancel(NULL),
@@ -574,7 +574,7 @@ UX_STEP_VALID(
       "Reject if you're",
       "not sure",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_change_path_approval_flow_4_step,
     pb,
     io_seproxyhal_touch_display_ok(NULL),
@@ -606,7 +606,7 @@ UX_STEP_NOCB(
       .title = "Sign path",
       .text = vars.tmp_warning.derivation_path,
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_sign_path_approval_flow_3_step,
     pbb,
     io_seproxyhal_touch_sign_cancel(NULL),
@@ -615,7 +615,7 @@ UX_STEP_VALID(
       "Reject if you're",
       "not sure",
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_sign_path_approval_flow_4_step,
     pb,
     io_seproxyhal_touch_sign_ok(NULL),
@@ -655,7 +655,7 @@ UX_STEP_NOCB(
       .line1 = "or third party",
       .line2 = "wallet software"
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_segwit_input_approval_flow_4_step,
     pb,
     io_seproxyhal_touch_display_cancel(NULL),
@@ -663,7 +663,7 @@ UX_STEP_VALID(
       .icon = &C_icon_crossmark,
       .line1 = "Cancel"
     });
-UX_STEP_VALID(
+UX_STEP_CB(
     ux_request_segwit_input_approval_flow_5_step,
     pb,
     io_seproxyhal_touch_display_ok(NULL),
