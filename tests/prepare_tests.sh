eb1f8041e8aae764b2ac63c1ea9fba83fdc58ac4
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2021-04-09 14:25:27 +0200

    Add -j flags to make commands in prepare_tests.sh script

--- tests/prepare_tests.sh
+++ tests/prepare_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ..
 make clean
-make DEBUG=1  # compile optionally with PRINTF
+make -j DEBUG=1  # compile optionally with PRINTF
 mv bin/ tests/bitcoin-bin
 make clean
-make DEBUG=1 COIN=bitcoin_testnet
+make -j DEBUG=1 COIN=bitcoin_testnet
 mv bin/ tests/bitcoin-testnet-bin
