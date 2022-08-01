a0975a4946d5d4826a042f4b3afddb63df60dcec
Sarah GLINER <sarah.gliner@ledger.fr>
2022-08-01 16:21:05 +0200

    tests: add NU5 signature test

--- tests/test_sign.py
+++ tests/test_sign.py
@@ -14,8 +14,60 @@ from utils import automation
 
 
 @automation("automations/accept.json")
+def test_NU5_signature(cmd, transport):
+    TXID_LEN = 112
+    KEY_LEN = 268
+    SIG_LEN = 142
+    EXPECTED_SIG = "304402202b22627d88f9ecebf2ab586ffa970232cddad6eabb3289fa1359b2bc9f5554bc02207cfba5db7c01b89c5d540dcb1ada67d485ab1638c2151eaa78b4d368059c007801"
 
+    sw, _ = transport.exchange_raw("e04200000d00000000050000800a27a72601")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04280002598cd6cd9559cd98109ad0622f899bc38805f11648e4f985ebe344b8238f87b13010000006b")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04280003248304502210095104ae9d53a95105be4ba5a31caddff2ae83ced24b21ab4aec6d735d568fad102206e054b158047529bb736")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e042800032c810902ea7fc8d92f3f604c1b2a8bb0b92f0e6c016a8012102010a560c7325827df0212bca20f5cf6556b1345991b6b64b46")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04280000b9c616e758230a5ffffffff")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04280000102")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e0428000221595dd04000000001976a914ca3ba17907dde979bf4e88f5c1be0ddf0847b25d88ac")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e042800022a245117c140000001976a914c8b56e00740e62449a053c15bdd4809f720b5cb588ac")
+    assert sw == 0x9000
 
+    sw, txid = transport.exchange_raw("e0428000090000000004f9081a00")
+    txid = txid.hex()
+    assert sw == 0x9000
+    assert len(txid) == TXID_LEN
 
+    sw, key = transport.exchange_raw("e040000015058000002c80000085800000000000000000000002")
+    key = key.hex()
+    assert sw == 0x9000
+    assert len(key) == KEY_LEN
+    key = key[4:70]
 
+    sw, _ = transport.exchange_raw("e044000509050000800a27a72601")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04480053b0138" + txid + "00")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04480050400000000")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04a80002301958ddd04000000001976a91431352ad6f20315d1233d6e6da7ec1d6958f2bf1988ac")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04800000b0000000000000100000000")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e044008009050000800a27a72601")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04480803b0138" + txid + "19")
+    assert sw == 0x9000
+    sw, _ = transport.exchange_raw("e04480801d76a914ca3ba17907dde979bf4e88f5c1be0ddf0847b25d88ac00000000")
+    assert sw == 0x9000
+
+    sw, sig = transport.exchange_raw("e04800001f058000002c8000008580000000000000000000000200000000000100000000")
+    assert sw == 0x9000
+    sig = sig.hex()
+    assert len(sig) == SIG_LEN
+    assert sig == "304402202b22627d88f9ecebf2ab586ffa970232cddad6eabb3289fa1359b2bc9f5554bc02207cfba5db7c01b89c5d540dcb1ada67d485ab1638c2151eaa78b4d368059c007801"
 
