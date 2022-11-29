24a53f374376f268006c2c0592810c77965e71dc
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2022-11-29 22:21:04 +0100

    Update target for legacy testnet app in prepare_tests.sh

--- tests/prepare_tests.sh
+++ tests/prepare_tests.sh
@@ -4,5 +4,5 @@ make clean
 make -j DEBUG=1  # compile optionally with PRINTF
 mv bin/ tests/bitcoin-bin
 make clean
-make -j DEBUG=1 COIN=bitcoin_testnet
+make -j DEBUG=1 COIN=bitcoin_testnet_legacy
 mv bin/ tests/bitcoin-testnet-bin
