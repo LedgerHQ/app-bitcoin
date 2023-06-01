331c0593f417edf6a1ff6e83bf23ce4703f4de87
Sarah GLINER <sarah.gliner@ledger.fr>
2023-06-01 18:19:33 +0200

    zcash: fix tests

--- tests/test_get_pubkey.py
+++ tests/test_get_pubkey.py
@@ -7,76 +7,13 @@ def test_get_public_key(cmd):
     # legacy address
     pub_key, addr, bip32_chain_code = cmd.get_public_key(
         addr_type=AddrType.Legacy,
-        bip32_path="m/44'/1'/0'/0/0",
-        display=False
-    )
-
-    assert pub_key == bytes.fromhex("04"
-                                    "ee8608207e21028426f69e76447d7e3d5e077049f5e683c3136c2314762a4718"
-                                    "b45f5224b05ebbad09f43594b7bd8dc0eff4519a07cbab37ecc66e0001ab959a")
-    assert addr == "mz5vLWdM1wHVGSmXUkhKVvZbJ2g4epMXSm"
-    assert bip32_chain_code == bytes.fromhex("0322c8f681e7274e767cee09b8e41770e6d2afd504fd5f85dfaab3e1ff6cdfcc")
-
-    # P2SH-P2WPKH address
-    pub_key, addr, bip32_chain_code = cmd.get_public_key(
-        addr_type=AddrType.P2SH_P2WPKH,
-        bip32_path="m/49'/1'/0'/0/0",
-        display=False
-    )
-
-    assert pub_key == bytes.fromhex("04"
-                                    "a80f007194d53d37f6f99539f635390588c4e1c328b098295f61af40d60cb28a"
-                                    "7b5649e8121c89148fbab7d693108654685b4e939d9c1bc55a71b43f389929fe")
-    assert addr == "2MyHkbusvLomaarGYMqyq7q9pSBYJRwWcsw"
-    assert bip32_chain_code == bytes.fromhex("dc699bc018541f456df1ebd4dea516a633a6260e0a701eba143449adc2ca63f3")
-
-    # bech32 address
-    pub_key, addr, bip32_chain_code = cmd.get_public_key(
-        addr_type=AddrType.BECH32,
-        bip32_path="m/84'/1'/0'/0/0",
-        display=False
-    )
-
-    assert pub_key == bytes.fromhex("04"
-                                    "7cb75d34b005c4eb9f62bbf2c457d7638e813e757efcec8fa68677d950b63662"
-                                    "648e4f638cabc4e4383fa3fe8348456e46fa56742dcf500a5b50dc1d403492f0")
-    assert addr == "tb1qzdr7s2sr0dwmkwx033r4nujzk86u0cy6fmzfjk"
-    assert bip32_chain_code == bytes.fromhex("efd851020a3827ba0d3fd4375910f0ed55dbe8c5d740b37559e993b1d623a956")
-    # legacy address
-    pub_key, addr, bip32_chain_code = cmd.get_public_key(
-        addr_type=AddrType.Legacy,
-        bip32_path="m/44'/1'/0'/0/0",
+        bip32_path="m/44'/133'/0'/0/0",
         display=True
     )
 
     assert pub_key == bytes.fromhex("04"
-                                    "ee8608207e21028426f69e76447d7e3d5e077049f5e683c3136c2314762a4718"
-                                    "b45f5224b05ebbad09f43594b7bd8dc0eff4519a07cbab37ecc66e0001ab959a")
-    assert addr == "mz5vLWdM1wHVGSmXUkhKVvZbJ2g4epMXSm"
-    assert bip32_chain_code == bytes.fromhex("0322c8f681e7274e767cee09b8e41770e6d2afd504fd5f85dfaab3e1ff6cdfcc")
+                                    "749c3f99dd136601daa824ecf40ae144c1a7de432bf22dbb23c81c7b6077d431"
+                                    "da107d9809ccd29fe9ae29dee4d713a0cd16ceb88be6ab1c6b06ac7a4ee1daf2")
+    assert addr == "t1LBsxhHpmugntmxBVBNh6MSvq2CmUE6g9X"
+    assert bip32_chain_code == bytes.fromhex("8b6440cbb7301fb7f6f8d11ca87fbec223ad012abae58a3b3bb2a651f2d51dbe")
 
-    # P2SH-P2WPKH address
-    pub_key, addr, bip32_chain_code = cmd.get_public_key(
-        addr_type=AddrType.P2SH_P2WPKH,
-        bip32_path="m/49'/1'/0'/0/0",
-        display=True
-    )
-
-    assert pub_key == bytes.fromhex("04"
-                                    "a80f007194d53d37f6f99539f635390588c4e1c328b098295f61af40d60cb28a"
-                                    "7b5649e8121c89148fbab7d693108654685b4e939d9c1bc55a71b43f389929fe")
-    assert addr == "2MyHkbusvLomaarGYMqyq7q9pSBYJRwWcsw"
-    assert bip32_chain_code == bytes.fromhex("dc699bc018541f456df1ebd4dea516a633a6260e0a701eba143449adc2ca63f3")
-
-    # bech32 address
-    pub_key, addr, bip32_chain_code = cmd.get_public_key(
-        addr_type=AddrType.BECH32,
-        bip32_path="m/84'/1'/0'/0/0",
-        display=True
-    )
-
-    assert pub_key == bytes.fromhex("04"
-                                    "7cb75d34b005c4eb9f62bbf2c457d7638e813e757efcec8fa68677d950b63662"
-                                    "648e4f638cabc4e4383fa3fe8348456e46fa56742dcf500a5b50dc1d403492f0")
-    assert addr == "tb1qzdr7s2sr0dwmkwx033r4nujzk86u0cy6fmzfjk"
-    assert bip32_chain_code == bytes.fromhex("efd851020a3827ba0d3fd4375910f0ed55dbe8c5d740b37559e993b1d623a956")
