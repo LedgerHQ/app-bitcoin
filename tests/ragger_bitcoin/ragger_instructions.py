9b5f87709864aa5f09c2c408ab27be8e4c142d10
Sarah GLINER <sarah.gliner@ledger.fr>
2024-04-19 14:21:48 +0200

    Tests: update with latest changes

--- tests/ragger_bitcoin/ragger_instructions.py
+++ tests/ragger_bitcoin/ragger_instructions.py
@@ -42,30 +42,37 @@ class Instructions:
         self.new_request("Processing", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_TAP,
                          save_screenshot=save_screenshot)
 
-    def confirm_transaction(self, save_screenshot=True):
-        self.new_request("Sign", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_CONFIRM,
+    def review_start(self, output_count: int = 1, save_screenshot=True):
+        self.new_request("Review", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_TAP,
                          save_screenshot=save_screenshot)
-        self.new_request("TRANSACTION", NavInsID.USE_CASE_REVIEW_TAP,
-                         NavInsID.USE_CASE_STATUS_DISMISS,
+        for _ in range(0, output_count):
+            self.same_request("Amount", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_TAP,
+                         save_screenshot=save_screenshot)
+    def review_fees(self, fees_on_same_request: bool = True, save_screenshot=True):
+        if fees_on_same_request:
+            self.same_request("Fees", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_TAP,
+                         save_screenshot=save_screenshot)
+        else:
+            self.new_request("Fees", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_TAP,
                          save_screenshot=save_screenshot)
 
-    def same_request_confirm_transaction(self, save_screenshot=True):
+    def confirm_transaction(self, save_screenshot=True):
         self.same_request("Sign", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_CONFIRM,
                           save_screenshot=save_screenshot)
-        self.new_request("TRANSACTION", NavInsID.USE_CASE_REVIEW_TAP,
+        self.new_request("Transaction", NavInsID.USE_CASE_REVIEW_TAP,
                          NavInsID.USE_CASE_STATUS_DISMISS,
                          save_screenshot=save_screenshot)
 
     def confirm_message(self, save_screenshot=True):
         self.new_request("Sign", NavInsID.USE_CASE_REVIEW_TAP,
                          NavInsID.USE_CASE_REVIEW_CONFIRM, save_screenshot=save_screenshot)
-        self.new_request("MESSAGE", NavInsID.USE_CASE_REVIEW_TAP,
+        self.new_request("Message", NavInsID.USE_CASE_REVIEW_TAP,
                          NavInsID.USE_CASE_STATUS_DISMISS, save_screenshot=save_screenshot)
 
     def confirm_wallet(self, save_screenshot=True):
         self.new_request("Approve", NavInsID.USE_CASE_REVIEW_TAP, NavInsID.USE_CASE_REVIEW_CONFIRM,
                          save_screenshot=save_screenshot)
-        self.same_request("WALLET", NavInsID.USE_CASE_REVIEW_TAP,
+        self.same_request("Wallet", NavInsID.USE_CASE_REVIEW_TAP,
                           NavInsID.USE_CASE_STATUS_DISMISS, save_screenshot=save_screenshot)
 
     def reject_message(self, save_screenshot=True):
