5dfab583c15304bfd4b0f80d7e1faa28310588eb
Sarah GLINER <sarah.gliner@ledger.fr>
2024-05-02 11:48:07 +0200

    lib-app-bitcoin: replace with submodule

--- lib-app-bitcoin/swap/handle_check_address.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HANDLE_CHECK_ADDRESS_H_
-#define _HANDLE_CHECK_ADDRESS_H_
-
-#include "swap_lib_calls.h"
-
-void swap_handle_check_address(
-    check_address_parameters_t *check_address_params);
-
-#endif // _HANDLE_CHECK_ADDRESS_H_
