0f0e77e2a55ec2f5fee28e285391471c49c7eef8
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-02-05 17:48:30 +0100

    Modify Makefile and app dashboard to be generic

--- src/app_settings.c
+++
@@ -1,9 +0,0 @@
-#include "app_settings.h"
-
-#ifndef SKIP_FOR_CMOCKA
-// NVRAM storage for app settings. Initialized to all zeros by default.
-// Reset on application or OS update.
-const app_settings_t N_app_settings_real;
-#else
-uint8_t mock_allow_nondefault_sighash = 0;
-#endif
