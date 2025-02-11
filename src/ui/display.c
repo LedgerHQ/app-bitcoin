b5552daec5f8692dd76f2c5150388f49d5f86a67
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-02-11 17:11:05 +0100

    Expose io_ui_process

--- src/ui/display.c
+++ src/ui/display.c
@@ -64,7 +64,7 @@ void decrease_streaming_index(void) {
 
 // Process UI events until the current flow terminates; does not handle any APDU exchange
 // Returns true/false depending if the user accepted in the corresponding UX flow.
-static bool io_ui_process(dispatcher_context_t *context) {
+bool io_ui_process(dispatcher_context_t *context) {
     UNUSED(context);
     G_was_processing_screen_shown = false;
 
