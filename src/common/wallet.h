09c464958e054f805a7b3c2a2017227e38366f06
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2026-08-04 17:19:22 +0200

    Fix wrong comment in policy_node_thresh_t
    (cherry picked from commit fbbdca3f81f2c109690191044b07860dfeeb7135)

--- src/common/wallet.h
+++ src/common/wallet.h
@@ -395,7 +395,7 @@ typedef struct {
     int16_t k;                  // threshold
     int16_t n;                  // number of child scripts
     rptr_policy_node_scriptlist_t
-        scriptlist;  // pointer to array of exactly n pointers to child scripts
+        scriptlist;  // pointer to a linked list of exactly n child scripts
 } policy_node_thresh_t;
 
 typedef struct {
